a18a11fa277616aaa9b183669bdb5f3297847a37
luthfidev <luthfi.wek@gmail.com>
2022-04-19 23:08:51 +0700

    model users

---
+++ src/models/users.js
@@ -0,0 +1,21 @@
+'use strict';
+module.exports = function (sequelize, DataTypes) {
+    return sequelize.define('users', {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        name: DataTypes.STRING,
+        password: DataTypes.STRING,
+        email: DataTypes.STRING,
+        access_level_id : DataTypes.INTEGER
+    }, {
+        tableName: 'users',
+        timestamps: true,
+        createdAt: 'created_at',
+        updatedAt: 'updated_at',
+        paranoid: true,
+        deletedAt: 'deleted_at'
+    });
+};
